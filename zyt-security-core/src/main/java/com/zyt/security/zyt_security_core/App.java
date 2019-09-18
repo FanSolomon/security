95c5bcf3c75d7361c44aabeca8aeb39278eb6677
FanSolomon <jetonlyy@gmail.com>
2019-09-18 19:30:11 +0800

    系统框架搭建

---
+++ zyt-security-core/src/main/java/com/zyt/security/zyt_security_core/App.java
@@ -0,0 +1,13 @@
+package com.zyt.security.zyt_security_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
